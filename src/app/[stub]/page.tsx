df39c50deecf1f61f07ddaf41f07ba17b4e69cbc
arslasha <aliernhis@gmail.com>
2025-06-24 13:38:19 +0300

    fix: Link in stub

--- src/app/[stub]/page.tsx
+++ src/app/[stub]/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import styles from './stub.module.css';
+import Link from "next/link";
 
 export default function StubPage() {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Страница в разработке</h1>
             <p className={styles.subtitle}>Мы скоро всё добавим. Пока можно вернуться на главную.</p>
-            <a href="/" className={styles.button}>На главную</a>
+            <Link href="/" className={styles.button}>На главную</Link>
         </div>
     );
 }
