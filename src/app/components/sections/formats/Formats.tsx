4db376fb7b02f248f3f543a3a08af5f6b2458a96
arslasha <aliernhis@gmail.com>
2025-07-01 01:16:39 +0300

    fix: улучшен адаптив страниц, обновленно позиционирование

--- src/app/components/sections/formats/Formats.tsx
+++ src/app/components/sections/formats/Formats.tsx
@@ -5,36 +5,33 @@ import styles from './style.module.css';
 export default function Formats() {
     return (
         <div className={styles.container}>
-            {/* Фоновые слои */}
-            <div className={styles.bg}/>
-            <div> className={styles.fiber}</div>
-            {/* Текстовые секции */}
+            <div className={styles.bg} />
+            <div className={styles.fiber} />
             <div className={styles.text_container}>
                 <div className={styles.section}>
                     <div className={styles.heading}>
                         <div className={styles.sectionNumber}>01</div>
-                        <div className={styles.sectionTitle}><span className={styles.boldLetter}>Ф</span>ОРМАТЫ</div>
+                        <div className={styles.sectionTitle}>
+                            <span className={styles.boldLetter}>Ф</span>ОРМАТЫ
+                        </div>
                     </div>
                     <div className={styles.sectionContent}>
-                        <div className={styles.bullet}>Эксклюзивное театральное шоу
-                            по оригинальному сценарию</div>
-                        <div className={styles.bullet}>Иммерсивное шоу, в котором гости становятся участниками
-                            постановки
-                        </div>
+                        <div className={styles.bullet}>Эксклюзивное театральное шоу по оригинальному сценарию</div>
+                        <div className={styles.bullet}>Иммерсивное шоу, в котором гости становятся участниками постановки</div>
                     </div>
                 </div>
 
-                <div className={styles.section} style={{top: '260px'}}>
+                <div className={styles.section}>
                     <div className={styles.heading}>
                         <div className={styles.sectionNumber}>02</div>
-                        <div className={styles.sectionTitle}><span className={styles.boldLetter}>М</span>АЛАЯ ФОРМА
+                        <div className={styles.sectionTitle}>
+                            <span className={styles.boldLetter}>М</span>АЛАЯ ФОРМА
                         </div>
                     </div>
                     <div className={styles.sectionContent}>
                         <div className={styles.bullet}>Постановочный шоу-номер для любой вашей концепции</div>
                         <div className={styles.bullet}>Разработка и постановка Welcome</div>
-                        <div className={styles.bullet}>Насыщение события персонажами,
-                            поддерживающими повествование</div>
+                        <div className={styles.bullet}>Насыщение события персонажами, поддерживающими повествование</div>
                     </div>
                 </div>
             </div>
--- src/app/components/sections/home/Home.tsx
+++ src/app/components/sections/home/Home.tsx
@@ -5,12 +5,15 @@ import styles from './style.module.css';
 
 export default function Home() {
     return (
-            <div className={styles.container}>
-                <div className={styles.background}/>
-                <div className={styles.left}>
-                    <div className={styles.logo}/>
-                </div>
+        <div className={styles.container}>
+            <div className={styles.background}/>
+            <div className={styles.left}>
+                <div className={styles.logo}/>
+            </div>
+            <div className={styles.maskWrapper}>
                 <div className={styles.mask}/>
             </div>
+        </div>
+
     );
 }
